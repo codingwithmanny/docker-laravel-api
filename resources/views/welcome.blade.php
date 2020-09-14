27b9402120118ba34de99d0e79d63c745e838bb2
Manny <manny@His-Mac.local>
2020-09-14 15:56:22 -0400

    FEAT: added new title to welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Docker Laravel API
                 </div>
 
                 <div class="links">
